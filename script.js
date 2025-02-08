7b34287ad836752135569eb3c7df3ab6bfb9b5fd
vidit purohit <92804351+viz26@users.noreply.github.com>
2025-02-08 15:53:45 +0530

    box size

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function handleNoClick() {
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1}px`;
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
 }
 
 function handleYesClick() {
